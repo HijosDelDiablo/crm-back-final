5613e606eeb1a239aef3eba33666dc40a766461a
carlosPadillaTorres <carlos3.dp.t@gmail.com>
2025-11-29 16:55:16 -0600

    feat (getSalerByPeriod) Endpoint to get all sales of a specific period,  #29

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -6,10 +6,12 @@ import { Cotizacion, CotizacionSchema } from '../cotizacion/schemas/cotizacion.s
 import { Product, ProductSchema } from '../product/schemas/product.schema';
 import { User, UserSchema } from '../user/schemas/user.schema';
 import { CotizacionModule } from '../cotizacion/cotizacion.module';
+import { CompraModule } from 'src/compra/compra.module';
 
 @Module({
   imports: [
     CotizacionModule,
+    CompraModule,
     MongooseModule.forFeature([
       { name: Cotizacion.name, schema: CotizacionSchema }, 
       { name: Product.name, schema: ProductSchema },
