eaa4664a1364c7b5cdb22c11e889dccd844d6b8b
carlosPadillaTorres <carlos3.dp.t@gmail.com>
2025-11-29 23:20:04 -0600

    feat-Attribure vendedorQueAtiende was added to user schema to manage the relation between sellers and its clients

--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Document, Types } from 'mongoose';
 import { Rol } from '../../auth/enums/rol.enum';
 
 @Schema({ timestamps: true })
@@ -49,6 +49,9 @@ export class User extends Document {
 
   @Prop({ default: true })
   activo: boolean;
+
+   @Prop({ type: Types.ObjectId, ref: User.name })
+    vendedorQueAtiende?: Types.ObjectId; // Solo aplicable si el rol es CLIENTE
 }
 
 export type UserDocument = User & Document;
