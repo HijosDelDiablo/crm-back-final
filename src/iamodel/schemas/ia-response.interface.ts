1777a0d59096e027388492b6540afe09472fc3ee
MilkDev-bit <miltonpinamoreno@outlook.com>
2025-12-01 14:40:56 -0600

    ia}

--- src/iamodel/schemas/ia-response.interface.ts
+++ src/iamodel/schemas/ia-response.interface.ts
@@ -1,11 +1,13 @@
 export interface IaResponse {
   message: string;
-  type: 'text' 
+  type: String
       | 'cotizaciones_table' 
       | 'products_grid' 
       | 'clients_list' 
       | 'tasks_list'       
       | 'kpi_dashboard'    
       | 'expenses_table';
+  suggestion?: string;
+  metadata?: any;
   data?: any;
 }
